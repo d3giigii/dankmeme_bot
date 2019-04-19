b597ca3d558402883c2cb249f03f9be2500bfbda
d3giigii <lhammond997@gmail.com>
2019-04-18 21:28:01 -0400

    Invalid character fix

--- dankmeme_bot.py
+++ dankmeme_bot.py
@@ -5,6 +5,11 @@ import urllib.request
 import os
 import time
 import datetime
+import re
+
+# Invalid characters
+invalid_chars = ("<", ">", ":", "\"", "\/", "\\", "|", "?", "*")
+
 
 # Path to save images to. 
 PATH = "E:\\Media\\Pictures\\Memes\\Dankmemes"
@@ -18,25 +23,35 @@ print("Bot running. Parsing posts...")
 print("")
 
 # Iterate through posts in hot section of subreddit. Only iterates limit number of times. 
-for submission in subreddit.hot(limit=5):
+for submission in subreddit.hot(limit=60):
     valid_types = (".jpg", ".png") 
     url_string = str(submission.url) 
-    
+    title = str(submission.title)
+    safe_title = re.sub('\?', '', title)
+    safe_title = re.sub('*', '', title)
+    print(safe_title)
+
     if url_string.endswith(valid_types):
         # Get file type as string. 
         file_type = url_string[-4:]
 
         # Print title and url of submission in console. 
-        print("Title: " + submission.title)
+        print("Title: " + title)
         print("URL  : " + submission.url)
         print("-------------------------------------------")
 
         # Save the file to given path. 
-        full_filename = os.path.join(PATH, submission.title)
+        full_filename = re.sub('\?', '', os.path.join(PATH, title)) 
         urllib.request.urlretrieve(submission.url, full_filename + file_type)
-        pass
 
-    time.sleep(1)
+        # Add title and url to log file. 
+        try:
+            log_file = open("log.txt", "x")
+        except FileExistsError:
+            log_file = open("log.txt", "a")
+        log_file.write("[" + str(datetime.datetime.now()) + "] " + title + " : " + url_string + "\n")
+        pass
     pass
+    
 print("End of parse. ")
 print("")
